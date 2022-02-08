7890bf9ef0dd7035d43a0cd9c8e7ba59b85a5ad8
harish <poizonharish@gmail.com>
2022-02-08 23:30:24 +0530

    added new route /hey

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 
   res.send("<h1> Hello world....!")
 })
+
+app.get('/hey', (req, res) => {
+  res.send("<h1> different branch")
+})
 app.listen(4000, () => {
   console.log( 'server started at port 4000')
 })
